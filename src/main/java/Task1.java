16c23c0c3355a011f0e731ec584d163f5ff3b384
AlexandrNedilko <aleksandr_ned@ukr.net>
2019-06-04 23:32:39 +0300

    Add task1

--- src/main/java/Task1.java
+++ src/main/java/Task1.java
@@ -1,3 +1,19 @@
 //1. Create ArrayList and print it.
-public class Task1 {
-}
+
+import java.util.*;
+
+    public class Task1 {
+
+        public static void main(String[] args)
+        {
+            int element = 5;
+
+            ArrayList<Integer> mark = new ArrayList<Integer>();
+            for (int i = 0; i < element; i++)
+            {
+                mark.add(i, (int)(Math.random() * 10 + 1));
+            }
+            System.out.println(mark);
+
+        }
+    }
