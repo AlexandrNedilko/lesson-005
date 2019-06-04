4168b82de178526e2fceaa81450839136d2152e5
AlexandrNedilko <aleksandr_ned@ukr.net>
2019-06-05 00:03:57 +0300

    Add task2

--- src/main/java/Task2.java
+++ src/main/java/Task2.java
@@ -1,3 +1,12 @@
 //2. Create ENUM and print.
 public class Task2 {
-}
+         public static void main(String[] args) throws Exception {
+            System.out.println(Strings.Orc.name());
+            System.out.println(Strings.Halfling.name());
+            System.out.println(Strings.Heroes.name());
+        }
+    }
+
+    enum Strings {
+        Orc, Halfling, Heroes
+    }
