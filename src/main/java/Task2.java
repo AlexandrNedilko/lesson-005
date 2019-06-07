ed5abade91d7978dd3cd86c34fa5f7ffdac54070
AlexandrNedilko <aleksandr_ned@ukr.net>
2019-06-07 13:28:13 +0300

    Rename Halfling for Task2

--- src/main/java/Task2.java
+++ src/main/java/Task2.java
@@ -2,11 +2,11 @@
 public class Task2 {
          public static void main(String[] args) throws Exception {
             System.out.println(Strings.Orc.name());
-            System.out.println(Strings.Hafling.name());
+            System.out.println(Strings.Halfling.name());
             System.out.println(Strings.Heroes.name());
         }
     }
 
     enum Strings {
-        Orc, Hafling, Heroes
+        Orc, Halfling, Heroes
     }
