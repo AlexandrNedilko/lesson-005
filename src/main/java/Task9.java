72890fd77767e2c1bc6aff5d4f0da00d178b5f21
AlexandrNedilko <aleksandr_ned@ukr.net>
2019-06-07 12:44:07 +0300

    Add task 9

--- src/main/java/Task9.java
+++ src/main/java/Task9.java
@@ -1,35 +1,27 @@
 /*"9. Create ArrayList<Integer> and add 100 random numbers (Use Math.random()).
         Create HashSet<Integer> and add all unique numbers from the ArrayList."*/
+
+
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 
+public class Task9 {
 
-   import java.util.HashSet;
-
-   public class Task9 {
-       public static void main(String[] args) {
-
-           System.out.println("Random numbers between 1 and 100 are,");
-           for(int i=0; i < 5 ; i++)
-               System.out.println("Random Number ["+ (i+1) + "] : " + (int)(Math.random()*100));
-
-            // Create HashSet.
-            HashSet<String> hash = new HashSet<>();
-            hash.add("81");
-            hash.add("32");
-            hash.add("81"); // Duplicate element.
-            hash.add("51");
-            hash.add("99");
-            hash.add("34");
-
-            // Display size.
-            System.out.println(hash.size());
-
-            // See if these three elements exist.
-            System.out.println(hash.contains("81"));
-            System.out.println(hash.contains("32"));
-            System.out.println(hash.contains("51"));
-            System.out.println(hash.contains("99"));
-            System.out.println(hash.contains("34"));
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i=0; i<100; i++) {
+             list.add(1 + (int)(Math.random()*100));
+            System.out.print(list.get(i));
+            System.out.print(" ");
         }
+        System.out.println("");
+        System.out.println("******");
+        System.out.println("List size "+list.size());
+        HashSet<Integer> set = new HashSet<>(list);
+        System.out.println("set size "+set.size());
+        System.out.println("******");
     }
+}
+
+
